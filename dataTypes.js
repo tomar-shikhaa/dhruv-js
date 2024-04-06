ffdb13c044a47b9952d46cb8dac8ce6608809417
Shikha-tomar <shikha12techies@gmail.com>
2024-04-06 10:32:08 +0530

    first file

---
+++ dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" ; //treat all js code as newer version
+
+// alert("hello")  = we are using nodejs not browser
+let name = "shikha"
+let age = 22
+let isloggedin = false
+
+//  number = 2 to powe 54
+// bigint = for long number
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = abhi value asign ni hui h
+// symbol = component unique fine karne ke liye use hota h
+
+
+console.log(typeof age)
+console.log(typeof "")
---
+++ variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345
+let accountEmail ="shikha@gmail.com"
+var accountPassword = "2345"
+accountCity = "morena"
+
+accountEmail="swdefr"
+accountPassword= "56789"
+accountCity ="fgvbhnjmkl"
+let accountState;
+// accountId =3456
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+
+prefer not to use var because of issue in block scope and functional scope
+
+const ek bar define karne ke bad dubara change ni hota 
+{}= ye scope hote h  
+*/
